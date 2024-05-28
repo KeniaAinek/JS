55a5ce1e0ddcce052c29819a442fa10abd5730a9
Kenia Castro <keniad.ctorres@gmail.com>
2024-05-27 20:02:38 -0700

    cambios en practica.js

--- js/practica.js
+++ js/practica.js
@@ -23,15 +23,19 @@ function recorte(cadena, rec){
     if(typeof cadena !== "string"){
         console.log("Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso");
         let convert = cadena.toString();
-        let r = convert.slice(rec);
-        console.log(r);
+        
         
     }else{
-        let convert = cadena.toString();
-        let r = convert.slice(rec);
-        console.log(r);
-    }
+        const temp = "";
+        const nCadena = [];
+        for(let i = 0; i <= rec; i++){
+            for(let j = 0; j <= rec; j++ ){  
+               nCadena[j] = cadena[i]; 
+               //console.log(nCadena[]);      
+             }
 
+    }
+    }
 }
 
 recorte("Paraguas",5);
