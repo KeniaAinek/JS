eecc234e1109c838827d895f330e6bfbf22b9c66
Kenia Castro <keniad.ctorres@gmail.com>
2024-05-27 22:07:41 -0700

    segunda funcion terminada en Practica.js

--- js/practica.js
+++ js/practica.js
@@ -1,41 +1,50 @@
-//funcion contadora de caracteres en una cadena
-function caracters(cadena){
-    let caracteres;
-    if(typeof cadena !== "string"){
-        console.log("Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso");
-        let convert = cadena.toString();
-        for(let i = 0; i <= convert.length; i++){
-            caracteres = i++;
-        }
-        console.log(`La cadena introducida tiene: ${caracteres}`);
-    }else{
-        for(let i = 0; i <= cadena.length; i++){
-            caracteres = i++;
-        }
-        console.log(`La cadena introducida tiene: ${caracteres}`);
+// 1 funcion contadora de caracteres en una cadena
+function caracters(cadena) {
+  let caracteres;
+  if (typeof cadena !== "string") {
+    console.log(
+      "Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso"
+    );
+    let convert = cadena.toString();
+    for (let i = 0; i <= convert.length; i++) {
+      caracteres = i++;
     }
+    console.log(`La cadena introducida tiene: ${caracteres}`);
+  } else {
+    for (let i = 0; i <= cadena.length; i++) {
+      caracteres = i++;
+    }
+    console.log(`La cadena introducida tiene: ${caracteres}`);
+  }
 }
 
 caracters(1994);
 
-//Funcion que recorta cadena en el total de caracteres pedido
-function recorte(cadena, rec){
-    if(typeof cadena !== "string"){
-        console.log("Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso");
-        let convert = cadena.toString();
-        
-        
-    }else{
-        const temp = "";
-        const nCadena = [];
-        for(let i = 0; i <= rec; i++){
-            for(let j = 0; j <= rec; j++ ){  
-               nCadena[j] = cadena[i]; 
-               //console.log(nCadena[]);      
-             }
-
+// 2 Funcion que recorta cadena en el total de caracteres pedido
+function recorte(cadena, rec) {
+  rec = rec - 1;
+  let nCadena = "";
+  if (typeof cadena !== "string") {
+    console.log("Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso");
+    let convert = cadena.toString();
+    for (let i = 0; i <= rec; i++) {
+        nCadena += convert[i];
     }
+    console.log(nCadena);
+  }
+  else {
+    for (let i = 0; i <= rec; i++) {
+      nCadena += cadena[i];
     }
+    console.log(nCadena);
+  }
 }
 
-recorte("Paraguas",5);
+recorte(33333333, 5);
+
+// 3 separar cadena por caracter
+function separador(cadena, sep){
+
+}
+
+separador("Turtle rabbit Jisoo Kim", ".");
