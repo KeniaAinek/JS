b04f3f162bee3c1919584c9334f1dbe6ab3cc24d
Kenia Castro <keniad.ctorres@gmail.com>
2024-05-29 20:06:47 -0700

    Practicas del 5-8 agregadas

--- js/practica.js
+++ js/practica.js
@@ -1,5 +1,5 @@
 // 1 funcion contadora de caracteres en una cadena
-function caracters(cadena) {
+/* function caracters(cadena) {
   if(!cadena){
     console.log("La cadena esta vacia")
   }
@@ -14,15 +14,24 @@ function caracters(cadena) {
     console.info(`La cadena ${cadena} tiene: ${cadena.length} caracteres`);
   }
 }
-}
+} */
+
+const caracters = (cadena,convert = cadena.toString()) => 
+  (!cadena)? 
+   console.log("La cadena esta vacia"): 
+   (typeof cadena !== "String")?
+   console.info(`Esto no es una cadena, se ha convertidoen cadena ${convert} tiene: ${convert.length} caracteres`):
+   console.info(`La cadena ${cadena} tiene: ${cadena.length} caracteres`);
 
 caracters("Hola estrella Evangeline");
 
 // 2 Funcion que recorta cadena en el total de caracteres pedido
-function recorte(cadena, ini, rec) {
-  //rec = rec - 1;
+/* function recorte(cadena, ini, rec) {
   let nCadena = "";
-  if (typeof cadena !== "string") {
+  if(!cadena){
+    console.log("La cadena esta vacia")
+  }else if (typeof cadena !== "string") {
+    
     console.log(
       "Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso"
     );
@@ -33,14 +42,20 @@ function recorte(cadena, ini, rec) {
     nCadena = cadena.substr(ini, rec);
     console.log(nCadena);
   }
-}
-
-recorte("Hola Kenia", 0, 5);
+} */
+const recorte = (cadena, rec) =>
+(!cadena)?
+  console.log("La cadena esta vacia"):
+  console.info(cadena.slice(0,rec));
+  
+recorte("Hola Kenia", 4);
 
 // 3 separar cadena por caracter
-function separador(cadena, sep) {
+/* function separador(cadena, sep) {
     let nCadena = "";
-    if (typeof cadena !== "string") {
+    if(!cadena){
+      console.log("La cadena esta vacia")
+    } else if (typeof cadena !== "string") {
       console.log(
         "Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso"
       );
@@ -51,14 +66,21 @@ function separador(cadena, sep) {
         nCadena = cadena.split(sep);
       console.log(nCadena);
     }
-}
+} */
+
+const separador = (cadena, sep) =>
+  (!cadena)?
+    console.log("La cadena esta vacia"):
+    console.info(cadena.split(sep));
 
 separador("Turtle rabbit Jisoo Kim", " ");
 
-// funcion que repite texto 
-function repetir(cadena, count){
+// 4 funcion que repite texto 
+/* function repetir(cadena, count){
     let nCadena = "";
-    if (typeof cadena !== "string") {
+    if(!cadena){
+      console.warn("La cadena esta vacia")
+    }else if (typeof cadena !== "string") {
       console.log(
         "Los caracteres que ingresaste no son de tipo cadena por lo que convertiran a cadena para hacer el proceso"
       );
@@ -69,6 +91,30 @@ function repetir(cadena, count){
       nCadena = cadena.repeat(count);
       console.log(nCadena);
     }
-}
+} */
+
+const repetir= (cadena, count) =>
+  (!cadena)?
+    console.log("La cadena esta vacia"):
+    console.info(cadena.repeat(count));
+
 
 repetir("Turtle rabbit Jisoo Kim", 7);
+
+//5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH"
+const reverso = (cadena) =>
+  (!cadena)?
+  console.log("La cadena esta vacia"):
+  console.info(cadena.split("").reverse().join(""));
+
+  reverso("Kenia");
+
+  //6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2
+
+
+  //7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
+  
+
+  //8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+
+
