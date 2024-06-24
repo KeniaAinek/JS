868ca2f7be61162ebdcbb302b9bed4157fdac025
Kenia Castro <keniad.ctorres@gmail.com>
2024-06-23 20:23:16 -0700

    Archivos dom.html y dom.js creados

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+/* console.log(document);
+
+let texto = "Hola, soy Kenia"
+
+const hablar = (texto) => speechSynthesis.
+speak(new SpeechSynthesisUtterance(texto));
+
+hablar(texto); */
+
+
+console.log("***********Elementos del Documento**********");
+console.log(window.document);
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
