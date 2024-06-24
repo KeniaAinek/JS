aa58ea0fd42c10c8e8c4214b5985776c3f26df56
Kenia Castro <keniad.ctorres@gmail.com>
2024-06-23 21:36:47 -0700

    cambios en archivos DOM

--- js/dom.js
+++ js/dom.js
@@ -8,7 +8,7 @@ speak(new SpeechSynthesisUtterance(texto));
 hablar(texto); */
 
 
-console.log("***********Elementos del Documento**********");
+/* console.log("***********Elementos del Documento**********");
 console.log(window.document);
 console.log(document);
 console.log(document.head);
@@ -20,4 +20,16 @@ console.log(document.links);
 console.log(document.images);
 console.log(document.forms);
 console.log(document.styleSheets);
-console.log(document.scripts);
+console.log(document.scripts); */
+
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByName("nombre"));
+console.log(document.getElementById("menu"));
+console.log(document.querySelector("#menu"));
+console.log(document.querySelector("a"));
+console.log(document.querySelectorAll("a"));
+console.log(document.querySelectorAll("a").length);
+document.querySelectorAll("a").forEach(el => console.log(el));
+console.log(document.querySelector(".card"));
+console.log(document.querySelector(".card")[2]);
