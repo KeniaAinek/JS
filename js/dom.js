869c4135e24674aee139e54cb13d1002c40e1fef
Kenia Castro <keniad.ctorres@gmail.com>
2024-06-24 19:53:18 -0700

    actualizado dom.js con atributos y data-atributos, estulos css

--- js/dom.js
+++ js/dom.js
@@ -22,7 +22,7 @@ console.log(document.forms);
 console.log(document.styleSheets);
 console.log(document.scripts); */
 
-console.log(document.getElementsByTagName("li"));
+/* console.log(document.getElementsByTagName("li"));
 console.log(document.getElementsByClassName("card"));
 console.log(document.getElementsByName("nombre"));
 console.log(document.getElementById("menu"));
@@ -33,3 +33,66 @@ console.log(document.querySelectorAll("a").length);
 document.querySelectorAll("a").forEach(el => console.log(el));
 console.log(document.querySelector(".card"));
 console.log(document.querySelector(".card")[2]);
+ */
+
+/* console.log(document.documentElement.lang);
+console.log(document.documentElement.getAttribute("lang"));
+console.log(document.querySelector(".link-dom").href);
+console.log(document.querySelector(".link-dom").getAttribute("href"));
+
+document.documentElement.lang = "es";
+console.log(document.documentElement.lang);
+document.documentElement.setAttribute("lang", "es-MX");
+
+const $linkDOM = document.querySelector(".link-dom");
+
+$linkDOM.setAttribute("target","_blank");
+$linkDOM.setAttribute("rel","noopener");
+$linkDOM.setAttribute("href","https://www.youtube.com");
+console.log($linkDOM.hasAttribute("rel"));
+console.log($linkDOM.removeAttribute("rel"));
+console.log($linkDOM.hasAttribute("rel")); */
+
+//Data-Atributes 
+/* console.log($linkDOM.getAttribute("data-description"));
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento");
+console.log($linkDOM.dataset.description);
+$linkDOM.dataset.description = "Suscribete a youtubey comparte";
+console.log($linkDOM.dataset.description); */
+
+const $linkDOM = document.querySelector(".link-dom");
+console.log($linkDOM.style);
+console.log($linkDOM.getAttribute("style"));
+console.log($linkDOM.style.backgroundColor);
+console.log($linkDOM.style.color);
+console.log(window.getComputedStyle($linkDOM));
+console.log(getComputedStyle($linkDOM).getPropertyValue("color"));
+$linkDOM.style.setProperty("text-decoration","none");
+$linkDOM.style.setProperty("display","block");
+$linkDOM.style.width = "50%";
+$linkDOM.style.textAlign = "center";
+$linkDOM.style.marginLeft = "auto";
+$linkDOM.style.marginRigth = "auto";
+$linkDOM.style.padding = "1rem";
+$linkDOM.style.borderRadius = ".5rem";
+
+console.log($linkDOM.style);
+console.log($linkDOM.getAttribute("style"));
+
+//Variables CSS-Custom Properties CSS
+const $html = document.documentElement,
+$body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+let varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
+
+console.log(varDarkColor,varYellowColor);
+
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+
+$html.style.setProperty("--dark-color", "#000");
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+$body.style.setProperty("background-color", varDarkColor);
