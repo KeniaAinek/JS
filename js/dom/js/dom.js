bc405db48b29e8d2e57bd355e566c325d2397dfb
Kenia Castro <keniad.ctorres@gmail.com>
2024-07-10 20:14:36 -0700

    Eventos de teclado terminado en seccion 2

--- js/dom/js/dom.js
+++ js/dom/js/dom.js
@@ -1,6 +1,7 @@
 //menu hamburguesa
 import hamburguerMenu from "./menu_hamburguesa.js";
 import {reloj, alarma} from "./reloj.js"
+import { shortcuts, moveBall } from  "./teclado.js";
 
 const d = document;
 
@@ -8,6 +9,11 @@ d.addEventListener("DOMContentLoaded", (e) =>{
     hamburguerMenu(".panel-btn",".panel",".menu a");
     reloj(".clock",".init-rel",".stop-rel");
     alarma("assets/alarm.mp3",".alarm-init",".alarm-stop");
+});
+
+d.addEventListener("keydown",(e) =>{
+    shortcuts(e);
+    moveBall(e,".ball",".stage");
 });
 
 
