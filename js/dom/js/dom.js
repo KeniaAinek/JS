112372a8b3d185f70f5271299fe0eb0474c5fd7a
Kenia Castro <keniad.ctorres@gmail.com>
2024-08-01 19:54:06 -0700

    btn scroll up terminado

--- js/dom/js/dom.js
+++ js/dom/js/dom.js
@@ -3,7 +3,7 @@ import hamburguerMenu from "./menu_hamburguesa.js";
 import {reloj, alarma} from "./reloj.js"
 import { shortcuts, moveBall } from  "./teclado.js";
 import { countdown } from "./countdown.js"; 
-import { scrollUp }  from "./scroll_up.js"; 
+import scrollUp from "./scroll_up.js";
 
 const d = document;
 
@@ -11,6 +11,7 @@ d.addEventListener("DOMContentLoaded", (e) =>{
     hamburguerMenu(".panel-btn",".panel",".menu a");
     reloj(".clock",".init-rel",".stop-rel");
     alarma("assets/alarm.mp3",".alarm-init",".alarm-stop");
+    scrollUp(".scroll-up");
 });
 
 d.addEventListener("keydown",(e) =>{
@@ -20,4 +21,4 @@ d.addEventListener("keydown",(e) =>{
 
 countdown(".contador", "2024,12,29");
 
-scrollUp(".scroll-up", 600);
+
