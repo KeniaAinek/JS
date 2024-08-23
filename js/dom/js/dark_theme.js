fa1a13554302843d25757b01b6afa3e86bbf0ad9
Kenia Castro <keniad.ctorres@gmail.com>
2024-08-22 20:15:09 -0700

    Changes on dark mode

--- js/dom/js/dark_theme.js
+++ js/dom/js/dark_theme.js
@@ -1,7 +1,10 @@
-let d = document;
+let d = document,
+state = localStorage.getItem('state');
+
 export default function darkTheme(btn, classDark) {
     const $themeBtn = d.querySelector(btn),
         $selectors = d.querySelectorAll("[data-dark]");
+        
 
         let moon = "🌙",
             sun = "☀️";
@@ -9,9 +12,13 @@ export default function darkTheme(btn, classDark) {
     d.addEventListener('click', (e) =>{
         if(e.target.matches(btn)){ 
             if($themeBtn.textContent === moon){
-                $selectors.foreach(el => el.classList.add(classDark));
+                $selectors.forEach(el => el.classList.add(classDark));
+                $themeBtn.textContent = sun;
+                localStorage.getItemItem('Btnstate', $themeBtn.textContent);
             }else{
-                $selectors.foreach(el => el.classList.remoque(classDark));
+                $selectors.forEach(el => el.classList.remove(classDark));
+                $themeBtn.textContent = moon;
+                state = $themeBtn.textContent;
             }
         }
     });
