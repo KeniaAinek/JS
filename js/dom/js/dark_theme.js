0094569e459a599f3cc9b2a8088c884d72b4000f
Kenia Castro <keniad.ctorres@gmail.com>
2024-10-21 20:01:15 -0700

    resposive designe

--- js/dom/js/dark_theme.js
+++ js/dom/js/dark_theme.js
@@ -1,37 +1,40 @@
 let d = document,
-ls = localStorage;
-export default function darkTheme(btn, classDark) {
+    ls = localStorage;
+
+let moon = "🌙",
+    sun = "☀️";
+
+const lightMode = (btn, classDark) => {
     const $themeBtn = d.querySelector(btn),
         $selectors = d.querySelectorAll("[data-dark]");
-        
+    $selectors.forEach(el => el.classList.remove(classDark));
+    $themeBtn.textContent = moon;
+    ls.setItem("theme", "light");
+}
 
-        let moon = "🌙",
-            sun = "☀️";
+const darkMode = (btn, classDark) => {
+    const $themeBtn = d.querySelector(btn),
+        $selectors = d.querySelectorAll("[data-dark]");
+    $selectors.forEach(el => el.classList.add(classDark));
+    $themeBtn.textContent = sun;
+    ls.setItem("theme", "dark");
+}
 
-        const lightMode = () =>{
-            $selectors.forEach(el => el.classList.remove(classDark));
-            $themeBtn.textContent = moon;
-            ls.setItem("theme", "light");
-        }
+export default function darkTheme(btn, classDark) {
+    const $themeBtn = d.querySelector(btn);
 
-        const darkMode = () => {
-            $selectors.forEach(el => el.classList.add(classDark));
-            $themeBtn.textContent = sun;
-            ls.setItem("theme", "dark");
-        }
-        
-    d.addEventListener('click', (e) =>{
-        if(e.target.matches(btn)){ 
-            if($themeBtn.textContent === moon){
-                darkMode();
-            }else{
-                lightMode();
-        }
+    d.addEventListener('click', (e) => {
+        if (e.target.matches(btn)) {
+            if ($themeBtn.textContent === moon) {
+                darkMode(btn, classDark);
+            } else {
+                lightMode(btn, classDark);
+            }
         }
     });
 }
 
-d.addEventListener("DOMContentLoaded", (e) =>{
-    if(ls.getItem("theme") === null)ls.setItem("theme", "light");
-    (ls.getItem('theme') === "light") ? lightMode() : darkMode();
+d.addEventListener("DOMContentLoaded", (e) => {
+    if (ls.getItem("theme") === null) ls.setItem("theme", "light");
+    (ls.getItem('theme') === "light") ? lightMode("[data-dark-btn]", "dark-mode-class") : darkMode("[data-dark-btn]", "dark-mode-class");
 });
--- js/dom/js/dom.js
+++ js/dom/js/dom.js
@@ -5,6 +5,7 @@ import { shortcuts, moveBall } from  "./teclado.js";
 import { countdown } from "./countdown.js"; 
 import scrollUp from "./scroll_up.js";
 import darkTheme from "./dark_theme.js";
+import resposiveMedia from "./objeto-resposive.js";
 
 const d = document;
 
@@ -14,6 +15,17 @@ d.addEventListener("DOMContentLoaded", (e) =>{
     reloj(".clock",".init-rel",".stop-rel");
     alarma("assets/alarm.mp3",".alarm-init",".alarm-stop");
     scrollUp(".scroll-up");
+    resposiveMedia(
+        'youtube', 
+        '(min-width: 1024px)', 
+        '<a href="https://youtu.be/ekr2nIex040" target="_blank" rel="noopener">Ver Video</a>', 
+        '<iframe width="664" height="480" src="https://www.youtube.com/embed/ekr2nIex040" title="ROSÉ &amp; Bruno Mars - APT. (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>');
+
+    resposiveMedia(
+        'gmaps', 
+        '(min-width: 1024px)', 
+        '<a href="https://maps.app.goo.gl/N8KMypVSy5ovJrdLA" target="_blank" rel="noopener">Ver Mapa</a>', 
+        '<iframe width="664" height="480" src="https://maps.app.goo.gl/N8KMypVSy5ovJrdLA" title="ROSÉ &amp; Bruno Mars - APT. (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>');
 });
 
 d.addEventListener("keydown",(e) =>{
