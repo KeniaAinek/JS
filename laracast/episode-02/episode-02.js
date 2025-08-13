362877edd114a2e1b5f25f40608c5591b3062a1c
k.castro <k.castro@mefasa.org>
2025-08-13 08:52:27 -0700

    laracast episode 2

---
+++ laracast/episode-02/episode-02.js
@@ -0,0 +1,30 @@
+//Types
+typeof declaredVar; //undefined
+
+//undefined
+var declaredVar;
+
+//String 
+var doubleQouted = "Kenia \""; //string
+var singleQouted = 'Kenia \''; //string
+
+//Number
+var numb1 = 1; //number
+var num2 = 1.123; //number
+
+
+//Boolean
+var trueValue = true; //bool
+var falseValue = false; //bool
+
+//Symbol
+
+//Object 
+var obj = {}; //object
+var nullValue = null; //object
+
+//Array (no associative)
+var arr = []; //array
+
+//Function
+var func = function() {}; //function
